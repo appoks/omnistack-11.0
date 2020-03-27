0e89549b6c28bdd2339b966e71172c064eca796f
appoks <johnathan.nunes@outlook.com>
2020-03-27 19:39:57 -0300

    Back-End da aplicação BeTheHero

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = express.Router();
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const AuthController = require('./controllers/AuthController')
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/login', AuthController.login);
+
+module.exports = routes;
